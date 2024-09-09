e6b31e8f557d9ba1305ed7890a460f118f29046a
Wai Yan Min Khant <waiyanminkhant49@gmail.com>
2024-09-09 23:28:21 +0630

    Fixed Webhook event name

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -84,7 +84,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({
