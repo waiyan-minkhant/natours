22e738e9532f5f1897f05e58ba13d0225fbfe2a3
Wai Yan Min Khant <waiyanminkhant49@gmail.com>
2024-09-10 00:13:16 +0630

    Fixed createBookingCheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourID,
+    client_reference_id: req.params.tourId,
     // Old version
     // line_items: [
     //   {
@@ -64,7 +64,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  console.log(session);
+  // console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.find({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
